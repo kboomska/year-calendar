97d076d547153f79258554e202d14b992d3fdd86
Aleksey Kuzmin <kboomska@gmail.com>
2024-04-12 18:19:54 +0300

    Implement SfCalendarTheme

--- lib/calendar/widget/calendar_screen.dart
+++ lib/calendar/widget/calendar_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
 
 import 'package:calendar/calendar/widget/month_calendar.dart';
 
@@ -19,32 +20,38 @@ class CalendarScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: LayoutBuilder(builder: (context, constraints) {
-          return GridView.builder(
-            itemCount: 12,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              childAspectRatio: 1.0,
-              crossAxisSpacing: 16.0,
-              mainAxisSpacing: 16.0,
-            ),
-            itemBuilder: (context, index) {
-              return MonthCalendar(
-                month: index + 1,
-                currentDate: today,
-                controller: yearController[index],
-                onTap: (calendarTapDetails) {
-                  // При выборе даты сбрасываем выбор для других месяцев.
-                  for (CalendarController monthController in yearController) {
-                    if (monthController != yearController[index]) {
-                      monthController.selectedDate = null;
+        child: SfCalendarTheme(
+          data: SfCalendarThemeData(
+            selectionBorderColor: Colors.lightBlueAccent,
+            todayHighlightColor: Colors.blueAccent,
+          ),
+          child: LayoutBuilder(builder: (context, constraints) {
+            return GridView.builder(
+              itemCount: 12,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+              ),
+              itemBuilder: (context, index) {
+                return MonthCalendar(
+                  month: index + 1,
+                  currentDate: today,
+                  controller: yearController[index],
+                  onTap: (calendarTapDetails) {
+                    // При выборе даты сбрасываем выбор для других месяцев.
+                    for (CalendarController monthController in yearController) {
+                      if (monthController != yearController[index]) {
+                        monthController.selectedDate = null;
+                      }
                     }
-                  }
-                },
-              );
-            },
-          );
-        }),
+                  },
+                );
+              },
+            );
+          }),
+        ),
       ),
     );
   }
